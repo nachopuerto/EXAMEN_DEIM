d92989f3a8f06bef08b13871db81f3bc84fa8109
Nacho Puerto <kneel93@gmail.com>
2016-12-20 21:11:44 +0100

    3 movimiento futbol
    intento de control de player 1

---
+++ Assets/Script/controlplayer1.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controlplayer1 : MonoBehaviour {
+	Rigidbody2D rigi;
+	public float velocidad = 5f;
+	public float power = 1f;
+	private Rigidbody2D rb;
+
+	void movimientoderecha(){
+		rigi = GetComponent<Rigidbody2D>();
+		rb.velocity = new Vector2 (velocidad + power, rb.velocity.y);
+		this.transform.localScale = new Vector3 (1, 1, 1);
+	}
+	void Update () { 
+		if (Input.GetKey (KeyCode.RightArrow)) { 
+			movimientoderecha ();
+		}
+	}
+
+
+	}
